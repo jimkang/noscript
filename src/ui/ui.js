00611ab4eea2db6325eb3bbf125088f057089e59
Jim Kang <jimkang@gmail.com>
2025-05-02 16:18:55 -0400

    Make the site we're on the default in the domain selector instead of ANY SITE in the custom settings page.

--- src/ui/ui.js
+++ src/ui/ui.js
@@ -705,7 +705,6 @@ var UI = (() => {
           return label.includes(":") ? label : `…${label}`;
         };
         for (let child; child = ctxSelect.firstChild;) child.remove();
-        ctxSelect.appendChild(entry("*", _("anySite")));
         if (this.mainDomain) {
           const key = row.contextMatch || Sites.optimalKey(this.mainUrl);
           ctxSelect.appendChild(entry(key, toLabel(key))).selected = !!row.contextMatch;
@@ -718,6 +717,9 @@ var UI = (() => {
             }
           }
         }
+
+        ctxSelect.appendChild(entry("*", _("anySite")));
+
         let handleSelection = () => {
           let selected = ctxSelect.querySelector("option:checked");
           ctxReset.disabled = !(selected && selected.value !== "*");
